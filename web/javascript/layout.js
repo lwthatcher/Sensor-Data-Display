caf01d2690966760b4c0de32f8e2124ab5c0611f
Lawrence Thatcher <lwthatcher@msn.com>
2015-05-06 16:05:56 -0600

    Added basic titles

--- web/javascript/layout.js
+++ web/javascript/layout.js
@@ -1,8 +1,14 @@
-d3.select("body").append("div")
-	.attr("class","gyro-div");
+var gyroscope = d3.select("body").append("div")
+				.attr("class","gyro-div");
 
-d3.select("body").append("div")
-	.attr("class","acc-div");
+var accelerometer = d3.select("body").append("div")
+				.attr("class","acc-div");
+
+gyroscope.append("h2")
+	.text("Gyroscope");
+
+accelerometer.append("h2")
+	.text("Accelerometer");
 
 generateGraph("div.gyro-div","../tsv/gyroscope.tsv");
 generateGraph("div.acc-div","../tsv/accelerometer.tsv");
